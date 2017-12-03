c1e70415cf676d29e12634f9dc1ba91b61686bf9
110 <110@qq.com>
2017-12-03 16:25:36 +0800

    ball over

--- Vue_Project/src/components/goods/GoodsInfo.vue
+++ Vue_Project/src/components/goods/GoodsInfo.vue
@@ -1,5 +1,12 @@
 <template>
     <div class="goodsinfo-container">
+        <transition
+            @before-enter="beforeEnter"
+            @enter="enter"
+            @after-enter="afterEnter">
+            <div class="ball" v-show="ballFlag" ref="ball"></div>
+        </transition>
+
         <!-- 商品轮播图区域 -->
         <div class="mui-card">
             <div class="mui-card-content">
@@ -20,7 +27,7 @@
                 <p>购买数量：<numbox></numbox></p>
                 <p>
                     <mt-button type="primary" size="small">立即购买</mt-button>
-                    <mt-button type="danger" size="small">加入购物车</mt-button>
+                    <mt-button type="danger" size="small" @click="addToShopCar">加入购物车</mt-button>
                 </p>
                 </div>
             </div>
@@ -90,7 +97,45 @@
             goComment(id) {
                 // 点击跳转到 评论页面
                 this.$router.push({ name: "goodscomment", params: { id } });
-            }
+            },
+            addToShopCar() {
+                // 添加到购物车
+                this.ballFlag = !this.ballFlag;
+            },
+            afterEnter(el) {
+                this.ballFlag = !this.ballFlag;
+            },
+            enter(el, done) {
+                el.offsetWidth;
+                /*  小球动画优化思路：
+                    1. 先分析导致 动画 不准确的 本质原因： 
+                        a、我们把小球最终位移到的 位置，已经局限在了某一分辨率下的 滚动条未滚动的情况下；
+                        b、只要分辨率和 测试的时候不一样，或者 滚动条有一定的滚动距离之后， 问题就出现了；
+                    3. 经过分析，得到结论：不能把 位置的 横纵坐标 直接写死了，而是应该 根据不同情况，动态计算这个坐标值；
+                    4. 经过分析，得出解题思路：
+                        a、先得到 徽标的 横纵 坐标，再得到 小球的 横纵坐标，
+                        b、然后 让 y 值 求差， x 值也求 差，得到的结果，就是横纵坐标要位移的距离
+                    5. 如何 获取 徽标和小球的 位置？？？   domObject.getBoundingClientRect()
+                */
+
+                // 获取小球的 在页面中的位置
+                const ballPosition = this.$refs.ball.getBoundingClientRect();
+                // 获取 徽标 在页面中的位置
+                const badgePosition = document
+                    .getElementById("badge")
+                    .getBoundingClientRect();
+
+                const xDist = badgePosition.left - ballPosition.left;
+                const yDist = badgePosition.top - ballPosition.top;
+
+                el.style.transform = `translate(${xDist}px, ${yDist}px)`;
+                // el.style.transform = "translate(93px, 230px)";
+                el.style.transition = "all 0.5s cubic-bezier(.4,-0.3,1,.68)";
+                done();
+            },
+            beforeEnter(el) {
+                el.style.transform = "translate(0, 0)";
+            },
         },
         components: {
             swiper,
